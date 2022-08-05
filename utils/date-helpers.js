c22f6399dd4aa519a5a60617e24d35f3be3a964b
Nicolas Broca <n.broca@gmail.com>
2022-08-06 08:53:49 +1000

    add padding to be ISO-moncul compliant

--- utils/date-helpers.js
+++ utils/date-helpers.js
@@ -1,7 +1,7 @@
 function today() {
   const date = new Date()
 
-  return `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate}`
+  return `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}`
 }
 
 module.exports = {
