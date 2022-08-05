7a38f44444ad56916e3192f569a22ed3940db5d0
Nicolas Broca <n.broca@gmail.com>
2022-08-05 21:40:26 +1000

    add deploy script

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
-import 'dotenv/config'
-import { SlashCommandBuilder, Routes } from 'discord.js';
-import { REST } from '@discordjs/rest'
+require('dotenv').config()
+const fs = require('node:fs')
+const path = require('node:path')
+const { Routes } = require('discord.js')
+const { REST } = require('@discordjs/rest')
 
 const {
   DISCORD_APP_ID,
@@ -8,12 +10,15 @@ const {
   DISCORD_BOT_TOKEN
 } = process.env
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = []
+const commandPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandPath, file)
+	const command = require(filePath)
+	commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
 
