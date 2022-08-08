89e54470fd7d80e4a6ecc0cf84df58b206c10564
Nicolas Broca <n.broca@gmail.com>
2022-08-09 09:23:29 +1000

    refactor project structure

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // retrieve the commands
 client.commands = new Collection()
-const commandsPath = path.join(__dirname, 'commands')
+const commandsPath = path.join(__dirname, 'src', 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
