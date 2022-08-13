9dcdd712097a3eb2e186b725d67c78f930bad7d1
Nicolas Broca <n.broca@gmail.com>
2022-08-14 09:32:27 +1000

    fix date format (remove time)

--- src/utils/date-helpers.js
+++ src/utils/date-helpers.js
@@ -2,7 +2,7 @@ const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
 
 function today() {
   const date = zonedTimeToUtc(new Date())
-  const formatedDate = format(utcToZonedTime(date, 'Europe/Paris'), 'yyyy-MM-dd HH:mm')
+  const formatedDate = format(utcToZonedTime(date, 'Europe/Paris'), 'yyyy-MM-dd')
 
   return formatedDate
 }
